c24aafe98122e991b94651f33a8570c62f358982
George Petrov <jokopetrov@gmail.com>
2020-03-17 18:56:11 +0200

    Initial Aurora Commit of TrevorAI Clone

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,20 @@
+class HomeController < ApplicationController
+  def racing
+  end
+
+  def sport
+  end
+
+  def electric
+  end
+
+  def press
+  end
+
+  def company
+  end
+
+  def investors
+  end
+  
+end
